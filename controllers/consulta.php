21444f555c4e0ca0d992a3328f3864d53cdf02f3
Bertinoti <jeffersonbertinoti@gmail.com>
2022-02-01 02:07:16 +0100

    úpdate

--- controllers/consulta.php
+++ controllers/consulta.php
@@ -17,5 +17,62 @@ class Consulta extends Controller{
 
     }
 
+    function getEmployee($getId = null){
+        $id = $getId[0];
+        $employee= $this->model->getEmployeeId($id);
+        $this->view->employee = $employee;
+
+
+        $this->view->render( 'consulta/index');
+
+
+
+    }
+
+    function updateEmployee($getId){
+        $id = $getId[0];
+        $name           = $_POST['name'];
+        $last_name      = $_POST['last_name'];
+        $email          = $_POST['email'];
+        $gender_id      = $_POST['gender_id'];
+        $age            = $_POST['age'];
+        $phone_number   = $_POST['phone_number'];
+        $city           = $_POST['city'];
+        $street_address = $_POST['street_address'];
+        $state          = $_POST['state'];
+        $postal_code    = $_POST['postal_code'];
+
+        if($this->model->update([
+        'id'             => $getId[0],
+        'name'           => $name,
+        'last_name'      => $last_name,
+        'email'          => $email,
+        'gender_id'      => $gender_id,
+        'age'            => $age,
+        'phone_number'   => $phone_number,
+        'city'           => $city,
+        'street_address' => $street_address,
+        'state'          => $state,
+        'postal_code'    => $postal_code])){
+            // update employee
+
+        $this->view->render('consulta/index');
+        }else{
+            //error
+        }
+    }
+
+    function deleteEmployee($getId){
+        echo $getId;
+        $id= $getId[0];
+
+        // if($this->model->delete($id)){
+        //     echo 'deleted';
+        // }else{
+        //     echo 'Try Again';
+        // }
+        // $this->view->render('main/index');
+        $this->view->render( 'ayuda/index');
+    }
 
 }
--- models/consultamodel.php
+++ models/consultamodel.php
@@ -1,31 +1,109 @@
 <?php
 include_once 'models/empleado.php';
-class ConsultaModel extends Model{
+class ConsultaModel extends Model
+{
     public function __construct()
     {
         parent::__construct();
     }
-    public function get(){
+    public function get()
+    {
         $items = [];
-        try{
+        try {
             $query = $this->db->connect()->query("SELECT*FROM employees");
-            while($row = $query->fetch()){
+            while ($row = $query->fetch()) {
                 $item = new Empleado();
-                    $item->id             = $row['id'];
-                    $item->name           = $row['name'];
-                    $item->last_name      = $row['last_name'];
-                    $item->email          = $row['email'];
-                    $item->gender_id      = $row['gender_id'];
-                    $item->age            = $row['age'];
-                    $item->phone_number   = $row['phone_number'];
-                    $item->city           = $row['city'];
-                    $item->street_address = $row['street_address'];
-                    $item->state          = $row['state'];
-                    $item->postal_code    = $row['postal_code'];
-                    array_push($items, $item);
-                }
-                return $items;
-        }catch(PDOException $e){
+                $item->id             = $row['id'];
+                $item->name           = $row['name'];
+                $item->last_name      = $row['last_name'];
+                $item->email          = $row['email'];
+                $item->gender_id      = $row['gender_id'];
+                $item->age            = $row['age'];
+                $item->phone_number   = $row['phone_number'];
+                $item->city           = $row['city'];
+                $item->street_address = $row['street_address'];
+                $item->state          = $row['state'];
+                $item->postal_code    = $row['postal_code'];
+                array_push($items, $item);
+            }
+            return $items;
+        } catch (PDOException $e) {
+            return [];
+        }
+    }
+
+    public function getEmployeeId($id)
+    {
+        $items = [];
+        $query = $this->db->connect()->prepare("SELECT * FROM employees WHERE id = $id ");
+        try {
+            $query->execute();
+
+            while ($row = $query->fetch()) {
+                $item = new Empleado();
+                $item->id             = $row['id'];
+                $item->name           = $row['name'];
+                $item->last_name      = $row['last_name'];
+                $item->email          = $row['email'];
+                $item->gender_id      = $row['gender_id'];
+                $item->age            = $row['age'];
+                $item->phone_number   = $row['phone_number'];
+                $item->city           = $row['city'];
+                $item->street_address = $row['street_address'];
+                $item->state          = $row['state'];
+                $item->postal_code    = $row['postal_code'];
+                array_push($items, $item);
+            }
+            return $items;
+        } catch (PDOException $e) {
+            return [];
+        }
+    }
+
+    public function update($item)
+    {
+        print_r($item['name']);
+        $query = $this->db->connect()->prepare("UPDATE employees SET 
+                    name           = :name,
+                    last_name      = :last_name,
+                    email          = :email,
+                    gender_id      = :gender_id,
+                    age            = :age,
+                    phone_number   = :phone_number,
+                    city           = :city,
+                    street_address = :street_address,
+                    state          = :state,
+                    postal_code    = :postal_code
+            WHERE id = :id");
+
+        try {
+            $query->execute([
+                'id'             => $item['id'],
+                'name'           => $item['name'],
+                'last_name'      => $item['last_name'],
+                'email'          => $item['email'],
+                'gender_id'      => $item['gender_id'],
+                'age'            => $item['age'],
+                'phone_number'   => $item['phone_number'],
+                'city'           => $item['city'],
+                'street_address' => $item['street_address'],
+                'state'          => $item['state'],
+                'postal_code'    => $item['postal_code']
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            return [];
+        }
+    }
+
+    public function delete($id){
+        echo $id;
+        $query = $this->db->connect()->prepare("DELETE FROM employees WHERE id = $id ");
+
+        try {
+            $query->execute();
+            return true;
+        } catch (PDOException $e) {
             return [];
         }
     }
